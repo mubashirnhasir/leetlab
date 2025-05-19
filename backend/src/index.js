934f53e6889c105a709af27e32348da440d08c68
mubashirnhasir <mubashirnhasir@gmail.com>
2025-05-20 00:45:15 +0530

    feat: add authentication routes and implement user registration with JWT

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,18 @@
 import express from "express"
 import dotenv from 'dotenv'
+import cookieParser from "cookie-parser"
+
 import authRoutes from "./routes/auth.routes.js"
+
 
 
 dotenv.config()
 
 const port = process.env.PORT || 8081
 const app = express()
-app.use(express.json())
 
+app.use(express.json())
+app.use(cookieParser())
 
 app.get('/' , (req,res)=>{
     res.send("Hello guys welcome to leetlab")
