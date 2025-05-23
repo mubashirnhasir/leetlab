5886e6d7932b979a00d149fb81a077692a3e93f8
mubashirnhasir <mubashirnhasir@gmail.com>
2025-05-24 02:08:20 +0530

    refactor: improve executeCode function structure and enhance test case validation

--- backend/src/controllers/executeCode.controller.js
+++ backend/src/controllers/executeCode.controller.js
@@ -1,5 +1,44 @@
+import {pollBatchResults, submitBatch} from "../libs/judge0.libs.js"
 
+export const executeCode = async (req, res) => {
+    try {
+        const { source_code, language_id, stdin, expected_outputs, problemId } = req.body;
 
-export const executeCode = async(req,res)=>{
+        const userId = req.user.id;
+
+        //validating test cases
+
+        if (
+            !Array.isArray(stdin) ||
+            stdin.length === 0 ||
+            !Array.isArray(expected_outputs) ||
+            expected_outputs.length !== stdin.length
+        ) {
+            return res.status(400).json({ error: "Invalid or missing test cases" })
+        }
+
+        const submissions = stdin.map((input) => ({
+            source_code,
+            language_id,
+            stdin: input,
+        }))
+
+        const submitResponse = await submitBatch(submissions)
+
+        const tokens = submitResponse.map((res)=> res.token)
+
+        const results = await pollBatchResults(tokens)
+
+        console.log("---Results")
+        console.log(results);
         
+
+        res.status(200).json({
+            message:"Code Executed successfully"
+        })
+
+    } catch (error) {
+        return res.status(500).json({error:"Error Executing code"})
+    }
+
 }
