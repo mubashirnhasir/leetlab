67d554678729bf08d486fa9aeda428032d590e08
mubashirnhasir <mubashirnhasir@gmail.com>
2025-06-13 02:11:49 +0530

    feat: implement submission controller and routes for fetching submissions

--- backend/src/routes/submission.routes.js
+++ backend/src/routes/submission.routes.js
@@ -1,10 +1,13 @@
 import express from "express"
+import { authMiddleware } from "../middleware/auth.middleware.js"
+import { getAllSubmission, getAllTheSubmissionsForProblem, getSubmissionsForProblem } from "../controllers/submission.controller.js"
 
 const submissionRoutes = express.Router()
 
 
-
-
+submissionRoutes.get("/get-all-submissions", authMiddleware, getAllSubmission)
+submissionRoutes.get("/get-submissions/:problemId", authMiddleware, getSubmissionsForProblem)
+submissionRoutes.get("/get-submissions-count/:problemId", authMiddleware, getAllTheSubmissionsForProblem)
 
 
 export default submissionRoutes
